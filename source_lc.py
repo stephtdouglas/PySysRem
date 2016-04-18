b9da976b60b3aaa0b6f42fb55bfb860f3339dd5a
stephtdouglas <stephanietdouglas@gmail.com>
2016-04-18 14:31:47 -0400

    Pythonically import PTF light curve, and test output

--- source_lc.py
+++ source_lc.py
@@ -1,97 +1,57 @@
-import numpy
+import numpy as np
 
 class source:
-    def __init__(self, xc, yc=0, xs=[], ys=[], xschip=[], yschip=[], mags=[], magerrors=[], epochs=[], flags=[], images=[], filename=""):
-        if isinstance(xc, float) or isinstance(xc, int):
-            self.xc = xc
-            self.yc = yc
-            self.xschip = xschip
-            self.yschip = yschip
-            self.xs = xs
-            self.ys = ys
-            self.mags = mags
-            self.magerrors = magerrors
-            self.epochs = epochs
-            self.flags = flags
-            self.images = images
-            self.filename = filename
-            
-        if isinstance(xc, str): # a filename is specified
-            epochs = []
-            mags = []
-            mag_errs = []
-            xskys = []
-            yskys = []
-            xchips = []
-            ychips = []
-            flags = []
-            images = []
+    def __init__(self, xc, yc=0, xs=[], ys=[], xschip=[], yschip=[], mags=[], magerrors=[], epochs=[], flags=[], filename=""):
+        self.xc = xc
+        self.yc = yc
+        self.xschip = xschip
+        self.yschip = yschip
+        self.xs = xs
+        self.ys = ys
+        self.mags = mags
+        self.magerrors = magerrors
+        self.epochs = epochs
+        self.flags = flags
+        self.images = []
+        self.filename = filename
 
-            for l in open(xc,"r"):
-                if xc.split('/')[-1].find("sysrem") == -1:
-                  if len(l.strip()) > 0 and l[0] != '#':
-                    s = l.split()
-                    epochs.append(float(s[0]))
-                    mags.append(float(s[1]))
-                    mag_errs.append(float(s[2]))
-                    xskys.append(float(s[3]))
-                    yskys.append(float(s[4]))
-                    xchips.append(float(s[5]))
-                    ychips.append(float(s[6]))
-                    flags.append(float(s[7]))
-                    if len(s) == 9:
-                        images.append(s[8])
-                    else:
-                        images.append("")
-                else:
-                  if len(l.strip()) > 0 and l[0] != '#':
-                    s = l.split()
-                    epochs.append(float(s[0]))
-                    mags.append(float(s[2]))
-                    mag_errs.append(float(s[3]))
-                    xskys.append(float(s[4]))
-                    yskys.append(float(s[5]))
-                    xchips.append(float(s[6]))
-                    ychips.append(float(s[7]))
-                    flags.append(float(s[8]))
-                    if len(s) == 10:
-                        images.append(s[9])
-                    else:
-                        images.append("")
-                    
-
-            self.epochs = numpy.array(epochs)
-            self.mags = numpy.array(mags)
-            self.magerrors = numpy.array(mag_errs)
-            self.xs = numpy.array(xskys)
-            self.ys = numpy.array(yskys)
-            self.xschip = numpy.array(xchips)
-            self.yschip = numpy.array(ychips)
-            self.flags = numpy.array(flags)
-            self.xc = numpy.median(xskys)
-            self.yc = numpy.median(yskys)
-            self.images = numpy.array(images)
-            self.filename = xc
-        
     def good_epochs_only(self):
         f = self.flags
         return source(self.xc, self.yc, self.xs[f == 0], self.ys[f == 0], self.xschip[f == 0], self.yschip[f == 0], self.mags[f == 0], self.magerrors[f == 0], self.epochs[f == 0], self.flags[f == 0],self.images[f == 0], self.filename)
 
     def n_good_epochs(self):
         return len(self.flags[self.flags == 0])
-    
+
     def remove_epoch(self, epoch):
         if epoch in self.epochs:
-            epoch_n = numpy.where(self.epochs == epoch)[0][0]
-            self.xschip = numpy.array(list(self.xschip[0:epoch_n]) + list(self.xschip[epoch_n+1:]),dtype=numpy.float32)
-            self.yschip = numpy.array(list(self.yschip[0:epoch_n]) + list(self.yschip[epoch_n+1:]),dtype=numpy.float32)
-            self.xs = numpy.array(list(self.xs[0:epoch_n]) + list(self.xs[epoch_n+1:]),dtype=numpy.float32)
-            self.ys = numpy.array(list(self.ys[0:epoch_n]) + list(self.ys[epoch_n+1:]),dtype=numpy.float32)
-            self.mags = numpy.array(list(self.mags[0:epoch_n]) + list(self.mags[epoch_n+1:]),dtype=numpy.float32)
-            self.magerrors = numpy.array(list(self.magerrors[0:epoch_n]) + list(self.magerrors[epoch_n+1:]),dtype=numpy.float32)
-            self.epochs = numpy.array(list(self.epochs[0:epoch_n]) + list(self.epochs[epoch_n+1:]),dtype=numpy.float32)
-            self.flags = numpy.array(list(self.flags[0:epoch_n]) + list(self.flags[epoch_n+1:]),dtype=numpy.float32)
-            self.images = numpy.array(list(self.images[0:epoch_n]) + list(self.images[epoch_n+1:]))
+            epoch_n = np.where(self.epochs == epoch)[0][0]
+            self.xschip = np.array(list(self.xschip[0:epoch_n]) + list(self.xschip[epoch_n+1:]),dtype=np.float32)
+            self.yschip = np.array(list(self.yschip[0:epoch_n]) + list(self.yschip[epoch_n+1:]),dtype=np.float32)
+            self.xs = np.array(list(self.xs[0:epoch_n]) + list(self.xs[epoch_n+1:]),dtype=np.float32)
+            self.ys = np.array(list(self.ys[0:epoch_n]) + list(self.ys[epoch_n+1:]),dtype=np.float32)
+            self.mags = np.array(list(self.mags[0:epoch_n]) + list(self.mags[epoch_n+1:]),dtype=np.float32)
+            self.magerrors = np.array(list(self.magerrors[0:epoch_n]) + list(self.magerrors[epoch_n+1:]),dtype=np.float32)
+            self.epochs = np.array(list(self.epochs[0:epoch_n]) + list(self.epochs[epoch_n+1:]),dtype=np.float32)
+            self.flags = np.array(list(self.flags[0:epoch_n]) + list(self.flags[epoch_n+1:]),dtype=np.float32)
+            self.images = np.array(list(self.images[0:epoch_n]) + list(self.images[epoch_n+1:]))
         else:
             print "*"*20, "Tried to remove epoch", epoch, "but not present in list", self.epochs
-            
+
+    @classmethod
+    def from_ptf(cls,filename):
+        lc = np.loadtxt(filename,usecols=np.arange(8))
+
+        epochs = lc[:,0]
+        mags = lc[:,1]
+        magerrors = lc[:,2]
+        xskys = lc[:,3]
+        yskys = lc[:,4]
+        xschip = lc[:,5]
+        yschip = lc[:,6]
+        flags = lc[:,7]
+        xc = np.median(xskys)
+        yc = np.median(yskys)
+        # Right now it's not saving the image filenames, which should be fine
+
+        ptf_lc = cls(xc, yc, xskys, yskys, xschip, yschip, mags, magerrors, epochs, flags, filename)
+        return ptf_lc
---
+++ tests/test_ptf_lc.py
@@ -0,0 +1,52 @@
+""" Test the light curve files."""
+
+from __future__ import print_function, division
+import os
+import pytest
+
+import numpy as np
+import astropy.io.ascii as at
+import astropy.io.fits as fits
+
+from .. import source_lc
+
+data = os.path.join(os.path.dirname(__file__),"lc_00001_calib.txt")
+# Extract the original file for testing
+lc_orig = np.loadtxt(data,usecols=np.arange(8))
+
+def test_ptf_lc():
+    lc = source_lc.source.from_ptf(data)
+
+def test_ptf_fname_str():
+    lc = source_lc.source.from_ptf(data)
+    assert type(lc.filename) is str
+
+def test_ptf_filename():
+    lc = source_lc.source.from_ptf(data)
+    assert lc.filename==data
+
+def test_ptf_epochs():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.epochs==lc_orig[:,0])
+
+def test_ptf_mags():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.mags==lc_orig[:,1])
+
+def test_ptf_mag_errs():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.magerrors==lc_orig[:,2])
+
+def test_ptf_sky():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.xs==lc_orig[:,3])
+    assert np.all(lc.ys==lc_orig[:,4])
+
+def test_ptf_chips():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.xschip==lc_orig[:,5])
+    assert np.all(lc.yschip==lc_orig[:,6])
+
+def test_ptf_flags():
+    lc = source_lc.source.from_ptf(data)
+    assert np.all(lc.flags==lc_orig[:,7])
