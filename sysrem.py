3928c6ca5cacaf61e819778d148a083893b91984
stephtdouglas <stephanietdouglas@gmail.com>
2016-05-04 15:43:52 -0400

    removing unnecessary comments at the top

--- sysrem.py
+++ sysrem.py
@@ -2,11 +2,6 @@
 # light curves based on a paper by O. Tamuz, T. Mazeh and S. Zucker (2004)
 # titled "Correcting systematic effects in a large set of photometric light
 # curves".
-#
-# Originally written for PTF data by a student of Nick Law (we think), and
-# modified by Marcel Agueros
-#
-# Now modified by Stephanie Douglas for K2 data
 
 import sys
 import os.path
